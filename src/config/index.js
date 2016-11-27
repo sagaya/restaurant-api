2feeee2d922888559496cba41e10d497723be3b7
cosmo1234 <harpheezsagaya@gmail.com>
2016-11-27 21:32:10 +0100

    Initial commit

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+//default export
+export default {
+  "port": 3000,
+  "mongoUrl": "mongodb://localhost:27017/retaurant-api",
+  "bodyLimit": "100kb"
+};
---
+++ src/controller/restaurant.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+import { Router } from 'express';
+import Restaurant from '../model/restaurant';
+
+
+export default ({config,db}) =>{
+  let api = Router()
+  // for v1/restaurant/add
+  api.post('/add', (req,res)=>{
+    let newRest = new Restaurant();
+    newRest.name = req.body.name;
+    newRest.save(err=>{
+      if (err) {
+        res.send(err);
+      }else {
+        res.json({message: 'Restaurant saved successfully'});
+      }
+    })
+  })
+
+  //read- part /v1/restaurant
+  api.get('/', (req,res)=>{
+    Restaurant.find({}, (err,restaurants)=>{
+      if (err) {
+        res.send(err);
+      }else {
+        res.json(restaurants);
+      }
+    });
+  });
+
+  //search
+  //read- /v1/restaurant/:id
+  api.get('/:id', (req,res)=>{
+    Restaurant.findById(req.params.id, (err,restaurant)=>{
+      if (err) {
+        res.send(err)
+      }else {
+        res.json(restaurant);
+      }
+    });
+
+  });
+
+  //PUT- /V1/restaurant/:ID
+  api.put('/;id', (req,res)=>{
+    Restaurant.findById(req.params.id, (err,restaurant)=>{
+      if (err) {
+        res.send(err);
+      }else {
+        restaurant.name = req.body.name;
+        restaurant.save(err => {
+          if (err) {
+            res.send(err)
+          }else{
+            res.json({message:"Restaurant info updated"});
+          }
+        });
+
+      }
+    })
+  });
+
+  return api;
+}
